db1b93eb850e91339c4bb1ccb3961aee743142cb
tmartin <xtom2301@gmail.com>
2022-01-31 17:20:04 +0100

    setup

---
+++ src/firebase.js/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCggi8PGzToHy1JooKdGLklmHwtFfGBRdM',
+  authDomain: 'reading-list-d5e94.firebaseapp.com',
+  projectId: 'reading-list-d5e94',
+  storageBucket: 'reading-list-d5e94.appspot.com',
+  messagingSenderId: '330591464068',
+  appId: '1:330591464068:web:7ba0c6df7e808c4b4db0d1',
+};
