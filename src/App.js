f6e3b3f5953181c24ea782f71a6c14f9d4b4513c
tmartin <xtom2301@gmail.com>
2022-01-31 18:33:10 +0100

    route guards

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
-// components
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
+import { Redirect } from 'react-router-dom';
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div className='App'>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              {user && <Home />}
+              {!user && <Redirect to='/login' />}
+            </Route>
+            <Route path='/signup'>
+              {!user && <Signup />}
+              {user && <Redirect to='/' />}
+            </Route>
+            <Route path='/login'>
+              {!user && <Login />} {user && <Redirect to='/' />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
