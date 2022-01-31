f4c0280959c157360bb0793423c9ccf36ac442b8
tmartin <xtom2301@gmail.com>
2022-01-31 18:27:57 +0100

    auth context

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { auth } from '../firebase.js/config';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useAuthContext } from './useAuthContext';
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
+  const { dispatch } = useAuthContext();
 
   const login = (email, password) => {
     setError(null);
     signInWithEmailAndPassword(auth, email, password)
       .then(({ user }) => {
-        console.log({ user });
+        dispatch({ type: 'LOGIN', payload: user });
       })
       .catch(({ message }) => {
         setError({ message });
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { auth } from '../firebase.js/config';
 import { signOut } from 'firebase/auth';
+import { useAuthContext } from './useAuthContext';
 
 export const useLogout = () => {
+  const { dispatch } = useAuthContext();
+
   const logout = () => {
     signOut(auth)
       .then(() => {
-        console.log('user logged out');
+        dispatch({ type: 'LOGOUT' });
       })
       .catch(({ message }) => {
         console.log(message);
